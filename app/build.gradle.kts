bc25d3c8aef2190c63ff03816198deb1a66eb9f3
flyoer5 <flyoer5@users.noreply.github.com>
2026-09-23 08:39:29 +0000

    chore(release): root 版 versionName 1.0-root -> 1.1-root

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0-root"
+        versionName = "1.1-root"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
